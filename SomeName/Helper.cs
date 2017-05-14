8fd4c653efb8dce7e9d06b2b42aaf4a62bbe966d
nguyendatz <nquocdat94@gmail.com>
2017-05-14 21:55:01 +0700

    Commit.

---
+++ SomeName/Helper.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows.Forms;
+
+namespace SomeName
+{
+    public static class Helper
+    {
+        public static bool IsValid<T>(this T obj) where T : class
+        {
+            var res = new ValidateResult();
+            var properties = obj.GetType().GetProperties();
+
+            foreach (var a in properties)
+            {
+                var atts = a.GetCustomAttributes(typeof(SomeName), true);
+                res.IsValid = true;
+
+                foreach (SomeName att in atts)
+                {
+                    res.IsValid = att.IsValid(a.GetValue(obj));
+
+                    if (!res.IsValid)
+                    {
+                        var err = att.ErrorMessage ?? string.Format("The field {0} is not valid.", a.Name);
+                        res.Error.Add(a.Name, err);
+                        break;
+                    }
+                }
+            }
+
+            if (res.IsValid)
+            {
+                MessageBox.Show("Sucess!");
+            }
+            else
+            {
+                foreach (var a in res.Error)
+                {
+                    MessageBox.Show(a.Value);
+                }
+            }
+
+            return res.IsValid;
+        }
+    }
+}
--- SomeName/SomeName.cs
+++ SomeName/SomeName.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Windows.Forms;
 
 namespace SomeName
 {
@@ -9,58 +7,5 @@ namespace SomeName
         public abstract bool IsValid(object input);
 
         public string ErrorMessage { get; set; }
-    }
-
-    public class ValidateResult
-    {
-        public ValidateResult()
-        {
-            Error = new Dictionary<string, string>();
-        }
-
-        public bool IsValid { get; set; }
-
-        public Dictionary<string, string> Error { get; set; }
-    }
-
-    public static class Helper
-    {
-        public static bool IsValid<T>(this T obj) where T: class
-        {
-            var res = new ValidateResult();
-            var properties = obj.GetType().GetProperties();
-
-            foreach(var a in properties)
-            {
-                var atts = a.GetCustomAttributes(typeof(SomeName), true);
-                res.IsValid = true;
-
-                foreach (SomeName att in atts)
-                {
-                    res.IsValid = att.IsValid(a.GetValue(obj));
-
-                    if (!res.IsValid)
-                    {
-                        var err = att.ErrorMessage ?? string.Format("The field {0} is not valid.", a.Name);
-                        res.Error.Add(a.Name, err);
-                        break;
-                    }
-                }
-            }
-
-            if (res.IsValid)
-            {
-                MessageBox.Show("Sucess!");
-            }
-            else
-            {
-                foreach (var a in res.Error)
-                {
-                    MessageBox.Show(a.Value);
-                }
-            }
-
-            return res.IsValid;
-        }
     }
 }
